0ce703b9dbd13726fb3ddac02b5e061ba0ffb088
raul1520 <raul.sanchez16@inacapmail.cl>
2023-05-27 04:49:02 +0000

    Consumo de API Pública

---
+++ mapquest_parse-json_1.py
@@ -0,0 +1,26 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "0gYtHsZwAZ8TBOV9NQfls5wvOeHpHSZS"
+
+while True:
+    orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+            break
+    dest = input("Destination: ")
+    if dest == "quit" or dest == "q":
+            break
+    url = main_api + urllib.parse.urlencode ({"key" :key, "from" :orig, "to" :dest})
+    print("URL: " + (url))
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + "= A successful route call.\n")
+        print("===============================================")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+        print("===============================================")
+
